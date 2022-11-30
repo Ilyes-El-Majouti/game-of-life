cfba12250740443f5a835d844f9e5377be4f4a3a
Ilyes-El-Majouti <ilyes.elmajouti@gmail.com>
2022-11-30 22:12:57 +0100

    jeu de la vie fini ajout de la version string optimale plus fonction get_grille, evolution_cell, evolution_jeu stable et optimale

--- jeu_de_la_vie.py
+++ jeu_de_la_vie.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 class JeuVie:
     def __init__(self, ligne, colonne):
@@ -13,6 +14,7 @@ class JeuVie:
         self.ligne = ligne
         self.colonne = colonne
         self.monde=[[random.randint(0,1) for x in range(self.colonne)] for y in range(self.ligne)]
+
     def __str__(self):
         """
             Affiche la grille avec les symboles graphiques du dictionnaire cells
@@ -25,7 +27,51 @@ class JeuVie:
             .....
             .....
         """
+        grille = ""
+        for ligne in self.monde:
+            for colonne in ligne:
+                if colonne:
+                    grille = grille + "#"
+                else:
+                    grille = grille + "."
+            grille = grille + "\n"
+        return grille
 
+    def get_grille(self, l, i):
+        if (0 <= i < self.ligne) and (0 <= l < self.colonne):
+            return self.monde[l][i]
+        else:
+            return False
 
-x = JeuVie(5,3)
-print(x)
+    def evolution_cell(self, ligne, colonne):
+        vivante = self.get_grille(ligne, colonne)
+        totale = sum(self.get_grille(ligne + ii, colonne + jj)
+                    for ii in [-1, 0, 1]
+                    for jj in [-1, 0, 1]
+                    if (ii, jj) != (0, 0))
+
+        if totale == 3:
+            celluleFutur = 1
+        elif totale < 2 or totale > 3:
+            celluleFutur = 0
+        else:
+            celluleFutur = vivante 
+        return celluleFutur
+
+    def evolution_jeu(self):
+        self.monde = [[self.evolution_cell(y, x) for x in range(self.colonne)] for y in range(self.ligne)]
+
+if __name__ == '__main__':
+    #Instanciation de l'objet grille avec 10 lignes et 10 colonnes
+    grille = JeuVie(10,10)
+    print(grille)
+    rep = 0
+    #repeter 20 fois
+    while rep <= 20:
+        print("Répétition= ",rep)
+        #appliquer une évolution du jeu à l'objet grille
+        grille.evolution_jeu()
+        time.sleep(0.2)
+        print(grille)
+        #Incrémenter la variable locale x
+        rep = rep + 1
